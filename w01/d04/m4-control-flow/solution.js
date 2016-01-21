dcaaef06ccb15b825cf6a6231514bba02376f3f8
Alex White <awhit012@gmail.com>
2016-01-21 15:23:13 -0800

    Update solution.js

--- w01/d04/m4-control-flow/solution.js
+++ w01/d04/m4-control-flow/solution.js
@@ -1,1 +1,13 @@
 
+for (i = 1; i < 101; i++) {
+
+  if((i % 3 === 0) && (i % 5 === 0)) {
+    console.log("fizzbuzz");
+  } else if(i % 3 === 0) {
+    console.log("fizz");
+  } else if(i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
