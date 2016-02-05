6d66793e080832ba8a5254bf5f2a3a4364745cf4
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-05 15:02:59 -0800

    merge sort lab in day 4

--- w03/d04/m1-merge-sort/solution-code/merge-sort.js
+++ w03/d04/m1-merge-sort/solution-code/merge-sort.js
@@ -25,16 +25,16 @@ function merge(left, right) {
   while (left.length && right.length) {
     // if the left array first element is less than the right array first element, push to result
     if (left[0] <= right[0]) {
-        result.push(left.shift());
+      result.push(left.shift());
     // else push the right array first element to result
     } else {
-        result.push(right.shift());
+      result.push(right.shift());
     }
   }
 
   // if left is the only array with elements, push them all in
   while (left.length) {
-      result.push(left.shift());
+    result.push(left.shift());
   // if right is the only array with elements, push them all in
   }
   while (right.length) {
