a09393e0747255153bdd16f862268c009d043b60
Alex White <awhit012@gmail.com>
2016-03-06 15:33:20 -0800

    adds passport lesson with explanation of passport and changed to handlebars

---
+++ w08/d02/m1-passport-and-oauth/starter-code/app/app.js
@@ -0,0 +1,31 @@
+var express        = require('express');
+var path           = require('path');
+var logger         = require('morgan');
+var bodyParser     = require('body-parser');
+var app            = express();
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var expressSession = require('express-session');
+var cookieParser   = require("cookie-parser");
+
+// Mongoose Setup
+mongoose.connect('mongodb://localhost:27017/facebook-authentication-app');
+
+// Middleware
+app.use( cookieParser() );
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// Setting up the Passport Strategies
+require("./config/passport")(passport)
+
+// Add code here:
+
+app.listen(3000);
