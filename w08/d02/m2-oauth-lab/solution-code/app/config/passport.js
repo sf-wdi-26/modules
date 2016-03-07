ce3298fd51907a065b1912eca3fba566af28f437
Alex White <awhit012@gmail.com>
2016-03-07 13:29:05 -0800

    cleans out old files

--- w08/d02/m2-oauth-lab/solution-code/app/config/passport.js
+++ w08/d02/m2-oauth-lab/solution-code/app/config/passport.js
@@ -1,69 +1,87 @@
 var User = require('../models/user');
+var API_CONFIG = require('../secrets')
 var FacebookStrategy = require('passport-facebook').Strategy;
+var GitHubStrategy = require('passport-github').Strategy;
+
 module.exports = function(passport){
-	passport.serializeUser(function(user, done) {
-      done(null, user._id);
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
   });
 
   passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
-          console.log('deserializing user:',user);
-          done(err, user);
-      });
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
   });
 
-
-
   passport.use('facebook', new FacebookStrategy({
-      clientID        : process.env.FACEBOOK_API_KEY,
-      clientSecret    : process.env.FACEBOOK_API_SECRET,
-      callbackURL     : 'http://localhost:3000/login/facebook/callback',
-      enableProof			: true,
-      profileFields   : ['name', 'emails']
-  },
+    clientID        : API_CONFIG.facebook.id,
+    clientSecret    : API_CONFIG.facebook.secret,
+    callbackURL     : 'http://localhost:3000/auth/facebook/callback',
+    enableProof     : true,
+    profileFields   : ['name', 'emails']
+  }, function(access_token, refresh_token, profile, done) {
 
-  // facebook will send back the tokens and profile
-  function(access_token, refresh_token, profile, done) {
+    // // Use this to see the information returned from Facebook
+    // console.log(profile)
 
-  	console.log('profile', profile);
+    process.nextTick(function() {
 
-		// asynchronous
-		process.nextTick(function() {
+      User.findOne({ 'fb.id' : profile.id }, function(err, user) {
+        if (err) return done(err);
+        if (user) {
+          return done(null, user);
+        } else {
 
-			// find the user in the database based on their facebook id
-	    User.findOne({ 'id' : profile.id }, function(err, user) {
-	    	// if there is an error, stop everything and return that
-	    	// ie an error connecting to the database
-	      if (err)
-	          return done(err);
+          var newUser = new User();
+          newUser.fb.id           = profile.id;
+          newUser.fb.access_token = access_token;
+          newUser.fb.firstName    = profile.name.givenName;
+          newUser.fb.lastName     = profile.name.familyName;
+          newUser.fb.email        = profile.emails[0].value;
 
-				// if the user is found, then log them in
-	      if (user) {
-	          return done(null, user); // user found, return that user
-	      } else {
-	        // if there is no user found with that facebook id, create them
-	        var newUser = new User();
+          newUser.save(function(err) {
+            if (err)
+              throw err;
 
-					// set all of the facebook information in our user model
-		      newUser.fb.id    				= profile.id; // set the users facebook id	                
-		      newUser.fb.access_token = access_token; // we will save the token that facebook provides to the user	                
-		      newUser.fb.firstName  	= profile.name.givenName;
-		      newUser.fb.lastName 		= profile.name.familyName; // look at the passport user profile to see how names are returned
-		      newUser.fb.email 				= profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+            return done(null, newUser);
+          });
+        }
 
-					// save our user to the database
-	        newUser.save(function(err) {
-	            if (err)
-	                throw err;
-
-	            // if successful, return the new user
-	            return done(null, newUser);
-	        });
-		    }
-
-	    });
-	  });
+      });
+    });
   }));
 
-}
+  passport.use(new GitHubStrategy({
+      clientID:     API_CONFIG.github.id,
+      clientSecret: API_CONFIG.github.secret,
+      callbackURL:  "http://127.0.0.1:3000/auth/github/callback"
+    },
+    function(access_token, refresh_token, profile, cb) {
 
+        process.nextTick(function() {
+
+          User.findOne({ 'fb.id' : profile.id }, function(err, user) {
+            if (err) return done(err);
+            if (user) {
+              return done(null, user);
+            } else {
+
+              var newUser = new User();
+              newUser.gh.id           = profile.id;
+              newUser.gh.access_token = access_token;
+              newUser.gh.name         = profile.name;
+              newUser.gh.email        = profile.emails[0].value;
+
+              newUser.save(function(err) {
+                if (err)
+                  throw err;
+
+                return done(null, newUser);
+              });
+            }
+
+          });
+        });
+      }))
+    }
--- w08/d02/m2-oauth-lab/solution-code/app/models/user.js
+++ w08/d02/m2-oauth-lab/solution-code/app/models/user.js
@@ -1,11 +1,18 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User',{
-	fb: {
-		id: String,
-		access_token: String,
-		firstName: String,
-		lastName: String,
-		email: String
-	}
+  fb: {
+    id: String,
+    access_token: String,
+    firstName: String,
+    lastName: String,
+    email: String
+  },
+  gh: {
+    id: String,
+    access_token: String,
+    firstName: String,
+    lastName: String,
+    email: String
+  }
 });
